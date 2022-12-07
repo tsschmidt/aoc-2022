fd20d6f5ad4cf9d51638dad1b98e989a8d79fda6
Travis Schmidt <travis.schmidt@gmail.com>
2022-12-07 09:06:03 -0800

    Day 6 Solution refactor

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,9 +1,17 @@
 fun main() {
 
+    fun findMarker0(input: String, size: Int): Int {
+        var index = 0
+        val chars = input.toList()
+        while(chars.subList(index, index + size).distinct().size < size) {
+            index++
+        }
+        return index + size
+    }
+    
     fun findMarker(input: String, size: Int): Int {
-        val wind = input.windowed(size)
         var count = size - 1
-        wind.find {
+        input.windowed(size).find {
             count++
             it.toList().distinct().size == size
         }
@@ -11,11 +19,11 @@ fun main() {
     }
     
     fun part1(input: List<String>): Int {
-        return findMarker(input[0], 4)
+        return findMarker0(input[0], 4)
     }
 
     fun part2(input: List<String>): Int {
-        return findMarker(input[0], 14)
+        return findMarker0(input[0], 14)
     }
 
 //    val input = readInput("Day06_test")
